4982daaa9b1ae3c65dd147c300dacb038740b19e
Mikita Kandratsyeu <nickondr.production@gmail.com>
2026-03-05 17:44:25 +0300

    chore: initial project

--- android/app/src/main/java/com/voiceinputapp/MainActivity.kt
+++ android/app/src/main/java/com/voiceinputapp/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "VoiceInputApp"
+  override fun getMainComponentName(): String = "VoiceInboxApp"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
