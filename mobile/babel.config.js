e2745165aa46101e19e85ff3448d8f3b98d92f87
Mikita Kandratsyeu <nickondr.production@gmail.com>
2026-03-24 22:44:07 +0300

    refactor: update Babel configuration for environment-specific dotenv handling
    - Modified the Babel configuration to dynamically select the environment file based on the NODE_ENV variable, enhancing flexibility for different environments.
    - Ensured the inclusion of the 'react-native-reanimated/plugin' in the plugins array for improved animation support.

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,29 +1,34 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
-  plugins: [
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        allowUndefined: true,
-      },
-    ],
-    [
-      'module-resolver',
-      {
-        root: ['.'],
-        alias: {
-          '@/': './src',
-          '@/app': './src/app',
-          '@/entities': './src/entities',
-          '@/features': './src/features',
-          '@/screens': './src/screens',
-          '@/shared': './src/shared',
-          '@/widgets': './src/widgets',
+module.exports = function (api) {
+  api.cache(() => process.env.NODE_ENV);
+  const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env';
+
+  return {
+    presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: envFile,
+          allowUndefined: true,
         },
-      },
+      ],
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@/': './src',
+            '@/app': './src/app',
+            '@/entities': './src/entities',
+            '@/features': './src/features',
+            '@/screens': './src/screens',
+            '@/shared': './src/shared',
+            '@/widgets': './src/widgets',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
     ],
-    'react-native-reanimated/plugin',
-  ],
+  };
 };
