d8acec2350872a1a5585b722ffa8bc32f8b2552c
Mikita Kandratsyeu <nickondr.production@gmail.com>
2026-03-28 13:28:08 +0300

    Rename local AI models to be more descriptive

--- mobile/src/entities/settings/model/constants.ts
+++ mobile/src/entities/settings/model/constants.ts
@@ -78,7 +78,7 @@ export const LOCAL_AI_MODELS: LocalAiModelCatalogEntry[] = [
   },
   {
     id: 'local/phi-3.5-mini-instruct-q4_k_m',
-    name: 'Phi-3.5 Mini',
+    name: 'Phi-3.5 Mini 4B',
     provider: 'Microsoft',
     descriptionKey: 'aiModels.localPhi35MiniDesc',
     speed: 'medium',
